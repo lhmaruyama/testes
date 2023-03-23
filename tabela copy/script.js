c4918690981e584d711452325fc52e63e0d5412c
Luís Henrique <lhmaruyama@gmail.com>
2023-03-23 18:14:09 -0300

    feat

--- tabela copy/script.js
+++ tabela copy/script.js
@@ -1,14 +1,62 @@
 function adicionarLinha() {
   let tabela = document.getElementById("tabela");
   let linha = tabela.insertRow(-1);
-  let coluna, celula;
+  let celula
+  //isActive.push(false)
+  //console.log(isActive);
+
   for (let i = 0; i < tabela.rows[0].cells.length; i++) {
-    coluna = linha.insertCell(i);
-    coluna.contentEditable = true;
-    //celula = document.createElement("td");
-    //coluna.appendChild(celula);
+
+    celula = linha.insertCell(i);
+    celula.contentEditable = true;
+    if (i == 0) {
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.className='checkbox'
+      celula.appendChild(checkbox);
+      //celula.innerHTML = '<input class="checkbox" type="checkbox">'
+    }
+    if(i == 1){
+      
+      celula.innerHTML =tabela.rows.length-1;
+    }
+    if (i <= 1) {
+      celula.style.backgroundColor = "#dddddd"
+    }
+
   }
 }
+
+function removerLinha() {
+  let tabela = document.getElementById("tabela");
+  let linhas = tabela.rows
+
+    if (tabela.rows.length > 1) {
+      tabela.deleteRow(-1);
+    }
+  //tabela.ariaRowIndex()
+  //isActive.pop()
+  //console.log(isActive);
+}
+
+
+
+
+
+
+//let isActive = [];
+//let isActive = [false, false, false, false, false, false, false, false, false, false]
+
+
+/* let checkboxes = document.querySelectorAll("input[type=checkbox]");
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("click", function () {
+    //isActive[i] = checkboxes[i].checked;
+    //console.log(isActive);
+    
+    //console.log(checkboxes.forEach(isActive=>{console.log(isActive)}));
+  });
+} */
 
 /* function adicionarColuna() {
   let tabela = document.getElementById("tabela");
@@ -21,13 +69,6 @@ function adicionarLinha() {
   }
 } */
 
-function removerLinha() {
-  let tabela = document.getElementById("tabela");
-/*   if (tabela.rows.length > 1) {
-    tabela.deleteRow(-1);
-  } */
-  tabela.ariaRowIndex()
-}
 
 /* function removerColuna() {
   let tabela = document.getElementById("tabela");
@@ -39,8 +80,8 @@ function removerLinha() {
 } */
 
 
-
-/* window.onload = function() {
+/* 
+window.onload = function() {
   var tabela = document.getElementById("tabela");
   var celulas = tabela.getElementsByTagName("td");
   for (var i = 0; i < celulas.length; i++) {
@@ -50,16 +91,28 @@ function removerLinha() {
   }
 }; */
 
-
+/* let selected = [false, false, false, false, false, false, false, false, false, false]
 for (let i = 0; i < tabela.rows.length; i++) {
   for (let j = 0; j < tabela.rows[i].cells.length; j++) {
-/*     tabela.rows[i].cells[j].addEventListener('click', function() {
-      console.log(`Linha ${i+1}, Coluna ${j+1}`);
-      this.style.backgroundColor = "yellow"
-    }); */
-    tabela.rows[i].cells[j].addEventListener('click', function() {
-      console.log(`Linha ${i+1}, Coluna ${j+1}`);
-      /* this.style.backgroundColor = "yellow" */
+    //tabela.rows[i].cells[j].addEventListener('click', function() {
+    // console.log(`Linha ${i+1}, Coluna ${j+1}`);
+    // this.style.backgroundColor = "yellow"
+    // });
+    tabela.rows[i].cells[0].addEventListener('click', function () {
+      //selected[i] = !selected[i]
+      //console.log(selected)
+      if (selected[i] == true) {
+        tabela.rows[i].style.backgroundColor = "yellow"
+        tabela.rows[i].cells[0].style.backgroundColor = "yellow"
+      } else {
+        tabela.rows[i].style.backgroundColor = ""
+        tabela.rows[i].cells[0].style.backgroundColor = "#dddddd"
+      }
+      
     });
   }
-}
+} */
+
+
+
+
